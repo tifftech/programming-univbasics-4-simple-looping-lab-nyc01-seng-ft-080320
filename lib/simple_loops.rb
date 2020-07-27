9b272517855965f6e0cf53509de27bd676c0def1
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-27 02:37:07 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,8 +6,12 @@ def loop_message_five_times(message)
   end
 end
 
-def loop_message_n_times(message, number)
-  
+def loop_message_n_times(message, limit)
+  count = 0
+  while count < limit do
+    puts message
+    count += 1
+  end
 end
 
 def
